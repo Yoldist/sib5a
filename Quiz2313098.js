4a6b0d33014d80dc9dd30fe8ee63bd13d3e6c4a4
Yoldist <yoldizoe436@gmail.com>
2025-10-30 09:36:32 +0800

    Add files via upload

---
+++ Quiz2313098.js
@@ -0,0 +1,48 @@
+class Karyawan {
+    constructor(nama,gaji){
+        this.nama = nama;
+        this.gaji = gaji;
+    }
+
+    hitungbonus(){
+        return 0;
+    }
+
+    getinfo() {
+        return `Nama: ${this.nama}, Gaji: ${this.gaji.toLocalString('id-ID')}`;
+    }
+}
+
+class KaryawanTetap extends Karyawan {
+
+    hitungbonus() {
+        return this.gaji * 0.10;
+    }
+}
+
+class KaryawanKontrak extends Karyawan {
+    hitungBonus() {
+        return this.gaji * 0.05;
+    }
+}
+
+const miyeon = new KaryawanTetap("Miyeon", 8000000);
+const minnie = new KaryawanKontrak("Minnie", 7000000);
+const soyeon = new KaryawanTetap("Soyeon", 6000000);
+const yuqi = new KaryawanKontrak("Yuqi", 5000000);
+const shuhua = new KaryawanTetap("Shuhua", 4000000);
+
+const daftarKaryawan = [miyeon,minnie,soyeon,yuqi,shuhua];
+
+let totalbonus = 0;
+
+console.log("--- Perhitungan Bonus Karyawan ---");
+
+daftarKaryawan.forEach(karyawan => {
+    const bonus = karyawan.hitungBonus();
+    totalBonus += bonus;
+    console.log(`${karyawan.getinfo()}, Jenis: ${karyawan.constructor.name}, Bonus: ${bonus.toLocaleString('id-ID')}`);
+});
+
+console.log("\n----------");
+console.log(`Total Bonus Seluruh Karyawan: ${totalBonus.toLocaleString('id-ID')}`);
